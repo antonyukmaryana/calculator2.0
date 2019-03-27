bcabf175a735f1192a4284ba325fe780ea9ead5a
Maryana Antonyuk and Tessa Sullivan <tessa@>
2019-03-27 14:26:44 -0700

    added subtraction, multiplication,division

--- js/scripts.js
+++ js/scripts.js
@@ -59,6 +59,27 @@ $(document).ready(function() {
     var result = add(number1, number2);
     $("#output").text(result);
   });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mul1").val());
+    var number2 = parseInt($("#mul2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
 });
 /*var height = parseFloat(prompt ("Enter Height"));
 var weight = parseFloat(prompt ("Enter Weight"));
