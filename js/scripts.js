6235cddd658edc525621675d422ba2e56730260a
Maryana Antonyuk and Tessa Sullivan <tessa@>
2019-03-27 13:58:03 -0700

    Added output controls.  NOTE This is not currently working, getting an error on the jquery.  Checked in due to time constraints.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-function addNum (num1, num2){
-  return num1 + num2;
-};
+// var addNum = function(num1, num2) {
+//   return num1 + num2;
+// };
 
 // function subNum (num1, num2){
 //   return num1 - num2;
@@ -25,15 +25,41 @@ function addNum (num1, num2){
 //   return fahrenheit;
 // }
 
+// $(document).ready(function() {
+//   $("form#add").submit(function(event) {
+//     event.preventDefault();
+//     var number1 = parseInt($("#number1").val());
+//     var number2 = parseInt($("#number2").val());
+//     var result = addNum(number1, number2);
+//     $("#output").text(result);
+//   });
+// });
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#number1").val());
-    var number2 = parseInt($("#number2").val());
-    alert(addNum(number1, number2));
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
   });
 });
-
 /*var height = parseFloat(prompt ("Enter Height"));
 var weight = parseFloat(prompt ("Enter Weight"));
 alert("Your BMI is " + BMI( weight, height));
