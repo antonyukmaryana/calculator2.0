b44987b1e89027c43139f0237a75d48cc4a37b50
Maryana Antonyuk and Tessa Sullivan <maryana@>
2019-03-27 11:34:29 -0700

    Added form controls to ask user for input

--- js/scripts.js
+++ js/scripts.js
@@ -2,34 +2,37 @@ function addNum (num1, num2){
   return num1 + num2;
 };
 
-function subNum (num1, num2){
-  return num1 - num2;
-};
+// function subNum (num1, num2){
+//   return num1 - num2;
+// };
+//
+// function divNum (num1, num2) {
+//   return num1 / num2;
+// };
+//
+// function multiNum (num1, num2){
+//   return num1 * num2;
+// };
+// /* BMI calculations*/
+// function BMI( weight, height){
+// 	return Math.pow(height,2);
+//
+// };
+// /*Convert celsius to fahrenheit*/
+//
+// function celsiusToFahrenheit (celsius){
+// 	var fahrenheit = 9/5 * celsius + 32;
+//   return fahrenheit;
+// }
 
-function divNum (num1, num2) {
-  return num1 / num2;
-};
-
-function multiNum (num1, num2){
-  return num1 * num2;
-};
-/* BMI calculations*/
-function BMI( weight, height){
-	return Math.pow(height,2);
-
-};
-/*Convert celsius to fahrenheit*/
-
-function celsiusToFahrenheit (celsius){
-	var fahrenheit = 9/5 * celsius + 32;
-  return fahrenheit;
-}
-
-var num1 = parseInt(prompt("enter a number"));
-var num2 = parseInt(prompt("enter another number"));
-alert(num1 + " * " + num2 + " = " + multiNum(num1,num2));
-
-
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    alert(addNum(number1, number2));
+  });
+});
 
 /*var height = parseFloat(prompt ("Enter Height"));
 var weight = parseFloat(prompt ("Enter Weight"));
